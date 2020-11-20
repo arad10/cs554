80c9558af502e0a489a2b7eea5838975e2d63b80
jessval <48558482+jessval@users.noreply.github.com>
2020-11-20 12:28:00 -0500

    basic structure to get us started

---
+++ src/components/Chat.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import ReactModal from 'react-modal';
+import Navbar from "./Navbar"
+
+export default function Chat(props) {
+
+  return (
+    <div>
+      <Navbar />
+      chat
+    </div>
+  )
+}
