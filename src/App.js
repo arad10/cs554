551ee1a0bba56e48bcc397a28261acca74a3ccbd
hrana2 <hrana2@stevens.edu>
2020-12-03 20:44:59 -0500

    prototype login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.scss';
+import firebase from "firebase"; 
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"; 
 import Home from "./components/Home"
 import Login from "./components/Login"
 import Dashboard from "./components/Dashboard"
 import Chat from "./components/Chat"
+import React, { Component } from 'react';
+
 
 import {
   BrowserRouter as Router,
@@ -10,17 +14,65 @@ import {
 } from "react-router-dom";
 
 
-function App() {
-  return (
-    <Router>
-      <Switch >
-      <Route exact path = "/" component = {Home}></Route>
-      <Route exact path = "/login" component = {Login}></Route>
-      <Route exact path = "/dashboard" component = {Dashboard}></Route>
-          <Route exact path = "/chat" component = {Chat}></Route>
-      </Switch>
-      </Router>
-  );
+// function App() {
+//   return (
+//     <Router>
+//       <Switch >
+//       <Route exact path = "/" component = {Home}></Route>
+//       <Route exact path = "/login" component = {Login}></Route>
+//       <Route exact path = "/dashboard" component = {Dashboard}></Route>
+//           <Route exact path = "/chat" component = {Chat}></Route>
+//       </Switch>
+//       </Router>
+//   );
+// }
+firebase.initializeApp({
+  apiKey: "AIzaSyAhARuwzHzD4Unv-r1-Y5zsUV6jeKYGNEM",
+  authDomain: "janch-130d6.firebaseapp.com"
+})
+
+class App extends Component {
+  state = {isSignedIn: false}
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => { 
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({isSignedIn:!!user})
+    })
+  }
+
+  render() {
+    return (
+        <div className="App">
+          {this.state.isSignedIn ? (
+            <Router>
+            <span>  
+            <button onClick={() => firebase.auth().signOut()}>Logout</button>
+            <p>Hello there</p>
+            </span>
+            </Router>
+          ) : ( 
+            <StyledFirebaseAuth
+              uiConfig = {this.uiConfig}
+              firebaseAuth = {firebase.auth()}
+              />
+          )}
+        </div>
+    )
+  }
 }
+
 
 export default App;
