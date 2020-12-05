65ea5281f2ba1a573ce64523ce38baa743a0cecb
rad710 <aradonci@stevens.edu>
2020-12-05 13:16:30 -0500

    Create dashboard APIs, set up routes to use dashboard APIs, and set up MongoDB

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const userRoutes = require('./users');
 const videochatRoutes = require('./videochat');
+const dashboardRoutes = require('./dashboard');
 
 const constructorMethod = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use('/users', userRoutes);
     app.use('/videochat', videochatRoutes);
+    app.use('/dashboard', dashboardRoutes);
 
     app.use('*', (req, res) => {
         res.status(404).json({ error: 'Not found' });
