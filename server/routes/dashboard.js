dba1af06615ca16231a7d4577d4657d593cdc2e5
rad710 <aradonci@stevens.edu>
2020-12-05 14:31:47 -0500

    Add await keyword on asynchronous operation

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -10,7 +10,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     console.log(req.body);
-    const insertDashboard = dashboardAPIs.addDashboard(
+    const insertDashboard = await dashboardAPIs.addDashboard(
         req.body.name,
         req.body.description,
         req.body.date,
