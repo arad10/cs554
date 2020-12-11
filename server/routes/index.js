762a20fb6aae01c3c2f79b98e118b045c42e7127
Nick Primamore <lancernp@gmail.com>
2020-12-10 23:14:41 -0500

    Fixed Heroku not found issue

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,10 +11,6 @@ const constructorMethod = (app) => {
     app.use('/userstory', userStoryRoutes);
     app.use('/videochat', videochatRoutes);
     app.use('/dashboard', dashboardRoutes);
-
-    app.use('*', (req, res) => {
-        res.status(404).json({ error: 'Not found' });
-    });
 };
 
 module.exports = constructorMethod;
