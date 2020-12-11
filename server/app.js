39b7eff6382fb75f8b7f01ae8f68f18a683f54df
Nick Primamore <lancernp@gmail.com>
2020-12-10 22:32:16 -0500

    Heroku setup in progress

--- server/app.js
+++ server/app.js
@@ -25,6 +25,14 @@ app.use(bodyParser.json());
 
 configRoutes(app);
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+});
+}
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
