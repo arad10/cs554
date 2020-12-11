9427fd98785cc7d130f5e3282201d9b1292e5920
Nick Primamore <lancernp@gmail.com>
2020-12-10 19:43:26 -0500

    Set up MongoDB Atlas (Cloud DB)

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 var logger = require('morgan');
 
 const configRoutes = require('./routes');
@@ -17,13 +18,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 configRoutes(app);
 
-app.listen(4000, () => {
-  console.log("We've now got a server!");
-  console.log("Your routes will be running on http://localhost:4000");
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
