56059ad3cf8cd87d63b7ea8adbfdd119d68878c3
Nick Primamore <lancernp@gmail.com>
2020-12-10 23:01:58 -0500

    Fixed build file location for Heroku

--- server/app.js
+++ server/app.js
@@ -26,10 +26,10 @@ app.use(bodyParser.json());
 configRoutes(app);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '../client', 'build')));
+  app.use(express.static(path.join(__dirname, '/../client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
 }
 
